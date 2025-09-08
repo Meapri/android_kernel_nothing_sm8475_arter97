99a0a64201580443fa15e773c2e05a94a5604dae
리지 <lizy@lijiui-MacBookPro.local>
2025-09-08 15:29:40 +0900

    ksu: add UDFPS bridge module and CI step to build udfpsd (NDK) + package zip

--- ksu-udfps-bridge/service.sh
+++ ksu-udfps-bridge/service.sh
@@ -1,4 +1,25 @@
 #!/system/bin/sh
+
+# Wait for sysfs nodes
+HBM_NODE="/sys/panel_feature/hbm_mode"
+FOD_NODE="/sys/devices/platform/goodix_ts.0/gesture/fod_en"
+TIMEOUT=15
+while [ $TIMEOUT -gt 0 ]; do
+  [ -w "$HBM_NODE" ] && [ -w "$FOD_NODE" ] && break
+  sleep 1
+  TIMEOUT=$((TIMEOUT-1))
+done
+
+# Enable FOD gesture once
+if [ -w "$FOD_NODE" ]; then
+  echo 1 > "$FOD_NODE"
+fi
+
+# Start userspace daemon
+chmod 0755 "$MODPATH/bin/udfpsd"
+exec "$MODPATH/bin/udfpsd" 2>&1 | logcat -s udfpsd &
+exit 0
+#!/system/bin/sh
 # Start low-overhead udfpsd (netlink/uevent based)
 MODPATH="${0%/*}"
 BIN="$MODPATH/system/bin/udfpsd"
